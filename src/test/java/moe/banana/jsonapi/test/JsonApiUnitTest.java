b4ab5c4617b2e7b6954f29f6470969b351a3af79
kirisetsz <kirisetsz@gmail.com>
2016-05-08 17:30:18 +0800

    refactor test

--- src/test/java/moe/banana/jsonapi/test/JsonApiUnitTest.java
+++ src/test/java/moe/banana/jsonapi/test/JsonApiUnitTest.java
@@ -10,6 +10,83 @@ import static org.junit.Assert.assertThat;
 @SuppressWarnings("ALL")
 public class JsonApiUnitTest {
 
+    private static final String JSON = "{\n" +
+            "  \"links\": {\n" +
+            "    \"self\": \"http://example.com/articles\",\n" +
+            "    \"next\": \"http://example.com/articles?page[offset]=2\",\n" +
+            "    \"last\": \"http://example.com/articles?page[offset]=10\"\n" +
+            "  },\n" +
+            "  \"data\": [{\n" +
+            "    \"type\": \"articles\",\n" +
+            "    \"id\": \"1\",\n" +
+            "    \"attributes\": {\n" +
+            "      \"title\": \"JSON API paints my bikeshed!\"\n" +
+            "    },\n" +
+            "    \"relationships\": {\n" +
+            "      \"author\": {\n" +
+            "        \"links\": {\n" +
+            "          \"self\": \"http://example.com/articles/1/relationships/author\",\n" +
+            "          \"related\": \"http://example.com/articles/1/author\"\n" +
+            "        },\n" +
+            "        \"data\": { \"type\": \"people\", \"id\": \"9\" }\n" +
+            "      },\n" +
+            "      \"comments\": {\n" +
+            "        \"links\": {\n" +
+            "          \"self\": \"http://example.com/articles/1/relationships/comments\",\n" +
+            "          \"related\": \"http://example.com/articles/1/comments\"\n" +
+            "        },\n" +
+            "        \"data\": [\n" +
+            "          { \"type\": \"comments\", \"id\": \"5\" },\n" +
+            "          { \"type\": \"comments\", \"id\": \"12\" }\n" +
+            "        ]\n" +
+            "      }\n" +
+            "    },\n" +
+            "    \"links\": {\n" +
+            "      \"self\": \"http://example.com/articles/1\"\n" +
+            "    }\n" +
+            "  }],\n" +
+            "  \"included\": [{\n" +
+            "    \"type\": \"people\",\n" +
+            "    \"id\": \"9\",\n" +
+            "    \"attributes\": {\n" +
+            "      \"first-name\": \"Dan\",\n" +
+            "      \"last-name\": \"Gebhardt\",\n" +
+            "      \"twitter\": \"dgeb\"\n" +
+            "    },\n" +
+            "    \"links\": {\n" +
+            "      \"self\": \"http://example.com/people/9\"\n" +
+            "    }\n" +
+            "  }, {\n" +
+            "    \"type\": \"comments\",\n" +
+            "    \"id\": \"5\",\n" +
+            "    \"attributes\": {\n" +
+            "      \"body\": \"First!\"\n" +
+            "    },\n" +
+            "    \"relationships\": {\n" +
+            "      \"author\": {\n" +
+            "        \"data\": { \"type\": \"people\", \"id\": \"2\" }\n" +
+            "      }\n" +
+            "    },\n" +
+            "    \"links\": {\n" +
+            "      \"self\": \"http://example.com/comments/5\"\n" +
+            "    }\n" +
+            "  }, {\n" +
+            "    \"type\": \"comments\",\n" +
+            "    \"id\": \"12\",\n" +
+            "    \"attributes\": {\n" +
+            "      \"body\": \"I like XML better\"\n" +
+            "    },\n" +
+            "    \"relationships\": {\n" +
+            "      \"author\": {\n" +
+            "        \"data\": { \"type\": \"people\", \"id\": \"9\" }\n" +
+            "      }\n" +
+            "    },\n" +
+            "    \"links\": {\n" +
+            "      \"self\": \"http://example.com/comments/12\"\n" +
+            "    }\n" +
+            "  }]\n" +
+            "}";
+
     public static Moshi moshi() {
         Moshi.Builder builder = new Moshi.Builder();
         builder.add(new JsonApiAdapterFactory());
@@ -20,83 +97,7 @@ public class JsonApiUnitTest {
 
     @Test
     public void jsonApiDocument_deserialization() throws Exception {
-        Document document = moshi().adapter(Document.class).fromJson(
-                "{\n" +
-                "  \"links\": {\n" +
-                "    \"self\": \"http://example.com/articles\",\n" +
-                "    \"next\": \"http://example.com/articles?page[offset]=2\",\n" +
-                "    \"last\": \"http://example.com/articles?page[offset]=10\"\n" +
-                "  },\n" +
-                "  \"data\": [{\n" +
-                "    \"type\": \"articles\",\n" +
-                "    \"id\": \"1\",\n" +
-                "    \"attributes\": {\n" +
-                "      \"title\": \"JSON API paints my bikeshed!\"\n" +
-                "    },\n" +
-                "    \"relationships\": {\n" +
-                "      \"author\": {\n" +
-                "        \"links\": {\n" +
-                "          \"self\": \"http://example.com/articles/1/relationships/author\",\n" +
-                "          \"related\": \"http://example.com/articles/1/author\"\n" +
-                "        },\n" +
-                "        \"data\": { \"type\": \"people\", \"id\": \"9\" }\n" +
-                "      },\n" +
-                "      \"comments\": {\n" +
-                "        \"links\": {\n" +
-                "          \"self\": \"http://example.com/articles/1/relationships/comments\",\n" +
-                "          \"related\": \"http://example.com/articles/1/comments\"\n" +
-                "        },\n" +
-                "        \"data\": [\n" +
-                "          { \"type\": \"comments\", \"id\": \"5\" },\n" +
-                "          { \"type\": \"comments\", \"id\": \"12\" }\n" +
-                "        ]\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"links\": {\n" +
-                "      \"self\": \"http://example.com/articles/1\"\n" +
-                "    }\n" +
-                "  }],\n" +
-                "  \"included\": [{\n" +
-                "    \"type\": \"people\",\n" +
-                "    \"id\": \"9\",\n" +
-                "    \"attributes\": {\n" +
-                "      \"first-name\": \"Dan\",\n" +
-                "      \"last-name\": \"Gebhardt\",\n" +
-                "      \"twitter\": \"dgeb\"\n" +
-                "    },\n" +
-                "    \"links\": {\n" +
-                "      \"self\": \"http://example.com/people/9\"\n" +
-                "    }\n" +
-                "  }, {\n" +
-                "    \"type\": \"comments\",\n" +
-                "    \"id\": \"5\",\n" +
-                "    \"attributes\": {\n" +
-                "      \"body\": \"First!\"\n" +
-                "    },\n" +
-                "    \"relationships\": {\n" +
-                "      \"author\": {\n" +
-                "        \"data\": { \"type\": \"people\", \"id\": \"2\" }\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"links\": {\n" +
-                "      \"self\": \"http://example.com/comments/5\"\n" +
-                "    }\n" +
-                "  }, {\n" +
-                "    \"type\": \"comments\",\n" +
-                "    \"id\": \"12\",\n" +
-                "    \"attributes\": {\n" +
-                "      \"body\": \"I like XML better\"\n" +
-                "    },\n" +
-                "    \"relationships\": {\n" +
-                "      \"author\": {\n" +
-                "        \"data\": { \"type\": \"people\", \"id\": \"9\" }\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"links\": {\n" +
-                "      \"self\": \"http://example.com/comments/12\"\n" +
-                "    }\n" +
-                "  }]\n" +
-                "}");
+        Document document = moshi().adapter(Document.class).fromJson(JSON);
         assertThat(document.jsonapi(), is(nullValue()));
         assertThat(document.links().last().href(), is("http://example.com/articles?page[offset]=10"));
         assertThat(document.links().self().href(), is("http://example.com/articles"));
