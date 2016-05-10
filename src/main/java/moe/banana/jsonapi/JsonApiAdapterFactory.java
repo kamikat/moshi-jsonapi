a75fd7c2c4f06d5a98cb0ef556ec72be700ee981
kirisetsz <kirisetsz@gmail.com>
2016-05-10 21:15:52 +0800

    refactor JsonApiAdapterFactory

--- src/main/java/moe/banana/jsonapi/JsonApiAdapterFactory.java
+++ src/main/java/moe/banana/jsonapi/JsonApiAdapterFactory.java
@@ -10,43 +10,6 @@ import java.util.Set;
 public class JsonApiAdapterFactory implements JsonAdapter.Factory {
 
     JsonAdapter.Factory[] mFactoryArray = new JsonAdapter.Factory[] {
-            new JsonAdapter.Factory() {
-                @Override
-                public JsonAdapter<?> create(Type type, Set<? extends Annotation> annotations, Moshi moshi) {
-                    if (type == Document.class) {
-                        return new DocumentJsonAdapter(moshi);
-                    }
-                    if (type == Link.class) {
-                        return new LinkJsonAdapter(moshi);
-                    }
-                    for (Annotation annotation : annotations) {
-                        if (annotation.annotationType() == Implicit.class) {
-                            if (type == Link.class) {
-                                return new LinkJsonAdapter(moshi);
-                            }
-                        }
-                        if (annotation.annotationType() == OneOrMany.class) {
-                            if (type == Resource.class) {
-                                return new OneOrManyJsonAdapter<>(Resource.class, new OneOrManyJsonAdapter.ContainerFactory<Resource>() {
-                                    @Override
-                                    public Resource createContainer() {
-                                        return new Resources();
-                                    }
-                                }, moshi);
-                            }
-                            if (type == ResourceLinkage.class) {
-                                return new OneOrManyJsonAdapter<>(ResourceLinkage.class, new OneOrManyJsonAdapter.ContainerFactory<ResourceLinkage>() {
-                                    @Override
-                                    public ResourceLinkage createContainer() {
-                                        return new ResourceLinkages();
-                                    }
-                                }, moshi);
-                            }
-                        }
-                    }
-                    return null;
-                }
-            },
             AutoValue_Document_Links.typeAdapterFactory(),
             AutoValue_Error.typeAdapterFactory(),
             AutoValue_Error_Links.typeAdapterFactory(),
@@ -61,6 +24,37 @@ public class JsonApiAdapterFactory implements JsonAdapter.Factory {
 
     @Override
     public JsonAdapter<?> create(Type type, Set<? extends Annotation> annotations, Moshi moshi) {
+        if (type == Document.class) {
+            return new DocumentJsonAdapter(moshi);
+        }
+        if (type == Link.class) {
+            return new LinkJsonAdapter(moshi);
+        }
+        for (Annotation annotation : annotations) {
+            if (annotation.annotationType() == Implicit.class) {
+                if (type == Link.class) {
+                    return new LinkJsonAdapter(moshi);
+                }
+            }
+            if (annotation.annotationType() == OneOrMany.class) {
+                if (type == Resource.class) {
+                    return new OneOrManyJsonAdapter<>(Resource.class, new OneOrManyJsonAdapter.ContainerFactory<Resource>() {
+                        @Override
+                        public Resource createContainer() {
+                            return new Resources();
+                        }
+                    }, moshi);
+                }
+                if (type == ResourceLinkage.class) {
+                    return new OneOrManyJsonAdapter<>(ResourceLinkage.class, new OneOrManyJsonAdapter.ContainerFactory<ResourceLinkage>() {
+                        @Override
+                        public ResourceLinkage createContainer() {
+                            return new ResourceLinkages();
+                        }
+                    }, moshi);
+                }
+            }
+        }
         for (JsonAdapter.Factory factory: mFactoryArray) {
             JsonAdapter<?> adapter = factory.create(type, annotations, moshi);
             if (adapter != null) {
