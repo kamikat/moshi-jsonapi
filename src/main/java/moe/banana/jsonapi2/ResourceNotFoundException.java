d215a3174581c75421197d40168236cc08232f86
kamikat <kirisetsz@gmail.com>
2016-08-18 15:24:50 +0800

    ResourceNotFoundException extends RuntimeException

--- src/main/java/moe/banana/jsonapi2/ResourceNotFoundException.java
+++ src/main/java/moe/banana/jsonapi2/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package moe.banana.jsonapi2;
 
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends RuntimeException {
 
     ResourceNotFoundException(String type, String id) {
         super("Resource " + type + "[id=" + id + "] could not be found.");
