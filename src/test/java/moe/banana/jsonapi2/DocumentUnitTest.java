7c2176edc0c241d835c3e164e0338375511b85f3
kamikat <kirisetsz@gmail.com>
2016-08-24 11:29:00 +0800

    add tests

--- src/test/java/moe/banana/jsonapi2/DocumentUnitTest.java
+++ src/test/java/moe/banana/jsonapi2/DocumentUnitTest.java
@@ -17,7 +17,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DocumentUnitTest {
 
-    private static final String JSON = "{" +
+    private static final String JSON_DATA_1 = "{" +
             "  \"links\": {" +
             "    \"self\": \"http://example.com/articles\"," +
             "    \"next\": \"http://example.com/articles?page[offset]=2\"," +
@@ -104,6 +104,26 @@ public class DocumentUnitTest {
             "  }]" +
             "}";
 
+    private static final String JSON_DATA_2 = "{" +
+            "  \"data\": {" +
+            "    \"type\": \"articles\"," +
+            "    \"id\": \"1\"," +
+            "    \"attributes\": {" +
+            "      \"title\": \"JSON API paints my bikeshed!\"" +
+            "    }" +
+            "  }," +
+            "  \"included\": [{" +
+            "    \"type\": \"unidentified\"," +
+            "    \"id\": \"9\"," +
+            "    \"attributes\": {" +
+            "      \"first-name\": \"Dan\"," +
+            "      \"last-name\": \"Gebhardt\"," +
+            "      \"twitter\": \"dgeb\"," +
+            "      \"age\": 20" +
+            "    }" +
+            "  }]" +
+            "}";
+
     public static Moshi moshi() {
         Moshi.Builder builder = new Moshi.Builder();
         builder.add(ResourceAdapterFactory.builder()
@@ -115,8 +135,8 @@ public class DocumentUnitTest {
     }
 
     @Test
-    public void deserialization() throws Exception {
-        Article[] articles = moshi().adapter(Article[].class).fromJson(JSON);
+    public void deserialize_array_of_object() throws Exception {
+        Article[] articles = moshi().adapter(Article[].class).fromJson(JSON_DATA_1);
         assertThat(articles, notNullValue());
         assertThat(articles.length, equalTo(1));
         Article a = articles[0];
@@ -128,7 +148,27 @@ public class DocumentUnitTest {
     }
 
     @Test
-    public void serialization() throws Exception {
+    public void deserialize_object() throws Exception {
+        Article article = moshi().adapter(Article.class).fromJson(JSON_DATA_2);
+        assertThat(article._id, equalTo("1"));
+        assertThat(article._type, equalTo("articles"));
+        assertThat(article.title, equalTo("JSON API paints my bikeshed!"));
+    }
+
+    @Test(expected = JsonDataException.class)
+    public void deserialize_strictly() throws Exception {
+        Moshi moshi = new Moshi.Builder()
+                .add(ResourceAdapterFactory.builder()
+                        .add(Article.class)
+                        .add(Person.class)
+                        .add(Comment.class)
+                        .strict()
+                        .build()).build();
+        moshi.adapter(Article.class).fromJson(JSON_DATA_2);
+    }
+
+    @Test
+    public void serialize_object() throws Exception {
         Document document = Document.create();
         Person author = new Person();
         author._id = "5";
@@ -147,36 +187,26 @@ public class DocumentUnitTest {
         assertThat(moshi().adapter(Article.class).toJson(article), equalTo("{\"data\":{\"type\":\"articles\",\"attributes\":{\"title\":\"Nineteen Eighty-Four\"},\"relationships\":{\"author\":{\"data\":{\"type\":\"people\",\"id\":\"5\"}},\"comments\":{\"data\":[{\"type\":\"comments\",\"id\":\"1\"}]}}},\"included\":[{\"type\":\"people\",\"id\":\"5\",\"attributes\":{\"first-name\":\"George\",\"last-name\":\"Orwell\"}},{\"type\":\"comments\",\"id\":\"1\",\"attributes\":{\"body\":\"Awesome!\"}}]}"));
     }
 
-    private static final String JSON2 = "{" +
-            "  \"data\": [{" +
-            "    \"type\": \"articles\"," +
-            "    \"id\": \"1\"," +
-            "    \"attributes\": {" +
-            "      \"title\": \"JSON API paints my bikeshed!\"" +
-            "    }" +
-            "  }]," +
-            "  \"included\": [{" +
-            "    \"type\": \"aliens\"," +
-            "    \"id\": \"9\"," +
-            "    \"attributes\": {" +
-            "      \"first-name\": \"Dan\"," +
-            "      \"last-name\": \"Gebhardt\"," +
-            "      \"twitter\": \"dgeb\"," +
-            "      \"age\": 20" +
-            "    }" +
-            "  }]" +
-            "}";
-
-    @Test(expected = JsonDataException.class)
-    public void deserialization_strictly() throws Exception {
-        Moshi moshi = new Moshi.Builder()
-                .add(ResourceAdapterFactory.builder()
-                        .add(Article.class)
-                        .add(Person.class)
-                        .add(Comment.class)
-                        .strict()
-                        .build()).build();
-        moshi.adapter(Article[].class).fromJson(JSON2);
+    @Test
+    public void serialize_array_of_object() throws Exception {
+        Document document = Document.create();
+        Person author = new Person();
+        author._id = "5";
+        author.firstName = "George";
+        author.lastName = "Orwell";
+        author.includeBy(document);
+        Comment comment1 = new Comment();
+        comment1._id = "1";
+        comment1.body = "Awesome!";
+        comment1.includeBy(document);
+        Article article = new Article();
+        article.title = "Nineteen Eighty-Four";
+        article.author = HasOne.create(article, author);
+        article.comments = HasMany.create(article, comment1);
+        article.addTo(document);
+        assertThat(
+                moshi().adapter(Article[].class).toJson(new Article[] { article }),
+                equalTo("{\"data\":[{\"type\":\"articles\",\"attributes\":{\"title\":\"Nineteen Eighty-Four\"},\"relationships\":{\"author\":{\"data\":{\"type\":\"people\",\"id\":\"5\"}},\"comments\":{\"data\":[{\"type\":\"comments\",\"id\":\"1\"}]}}}],\"included\":[{\"type\":\"people\",\"id\":\"5\",\"attributes\":{\"first-name\":\"George\",\"last-name\":\"Orwell\"}},{\"type\":\"comments\",\"id\":\"1\",\"attributes\":{\"body\":\"Awesome!\"}}]}"));
     }
 
 }
